f1cfb52c150e4ca6cc75cd70a1ac4a85f32c3815
karinastha <karinashrestha19@gmail.com>
2023-02-26 14:00:05 +0545

    done with nav bar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Header() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"> Design-Me </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Nav>
+          <Nav.Link href="#"> Home </Nav.Link>
+          <Nav.Link href="#memes"> About  </Nav.Link>
+          <Nav.Link href="#memes"> Contact  </Nav.Link>
+          <NavDropdown title="Customize Studio" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1"> T-shirt </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3"> Caps</NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
+        </Nav>
+
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
