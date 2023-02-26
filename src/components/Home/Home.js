d9538d1a192bc87c9e5141d33823b00c6a2de51b
karinastha <karinashrestha19@gmail.com>
2023-02-26 15:38:02 +0545

    Main section

---
+++ src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+
+
+function Home() {
+    return (
+        // <div className="home">
+        //     <div className="home__container">
+        //         {/* <div className="home__image"> Customize w' me </div> */}
+        //         <img src="https://t3.ftcdn.net/jpg/03/22/71/16/360_F_322711637_RhYLKPlbn5aj8kTnmOC4ULzsr0XcQSbh.jpg" alt="Img not found" className="home__image" />
+        //     </div>
+        // </div>
+        <div className="home">
+            <section className="home section">
+
+
+
+                <div className="home__data">
+                    <h1 className="home__title"> Customize w' us</h1>
+                    <p className="home__description">Order your favorite foods at any time and we will deliver them right to where you are.</p>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+export default Home;
