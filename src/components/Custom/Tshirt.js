ff7f2b22925e6b14a62e6923fa2387699cdaed29
Kushal Lal Shrestha <kushallalshrestha@rocketmail.com>
2023-03-02 18:11:31 +0545

    selected image displayed

--- src/components/Custom/Tshirt.js
+++ src/components/Custom/Tshirt.js
@@ -2,80 +2,89 @@ import { useState } from "react";
 import "./Tshirt.css";
 
 export function Tshirt() {
-    const data = { text: "" };
-    const [textInput, setText] = useState(data);
-    const [imageInput, setImage] = useState();
+  const [customTshirt, setCustomTshirt] = useState({ text: "", image: "" });
+  const [tshirts, setTshirts] = useState([]);
 
-    const handleSubmit = () => {
-        setText([...textInput]);
-        localStorage.setItem('formValues', JSON.stringify(textInput));
-        setImage([...imageInput]);
-        localStorage.setItem('formValues', JSON.stringify(imageInput));
-    };
+  const handleSubmit = () => {
+    // localStorage.setItem("formValues", JSON.stringify(customTshirt));
+    setTshirts([...tshirts, customTshirt]);
+  };
 
-    console.log(imageInput);
-    return (
-        <div className='main__box'>
-            <div className="first__box">
-                <h3> Welcome to Custom Gallery</h3>
-                <form className='form_data' method='post' enctype='multipart/form-data'>
-                    <div className="image">
-                        <input type="file"
-                            name="image"
-                            onChange={(e) =>
-                                this.setState({ imageInput, setImage: e.target.files[0] })
-                                // (e.target.) || setInputData({ ...inputData, name: e.target.[0] })
+  return (
+    <div className="main__box">
+      <div className="first__box">
+        <h3> Welcome to Custom Gallery</h3>
+        <form className="form_data" method="post" encType="multipart/form-data">
+          <div className="image">
+            <input
+              type="file"
+              name="image"
+              onChange={
+                (e) =>
+                  setCustomTshirt({
+                    ...customTshirt,
+                    image: URL.createObjectURL(e.target.files[0]),
+                  })
 
-                            }
-                        />
-                    </div>
-                    <div className="text">
-                        <input
-                            type="text"
-                            placeholder="Add text"
-                            name="fulltext"
-                            onChange={(e) =>
-                                textInput({ ...textInput, name: e.target.value })
-                            }
-                        ></input>
-                    </div>
-                    <div className="submit_btn">
-                        <button type="button" onClick={() => handleSubmit()}>
-                            Add
-                        </button>
-                        <div>
-                            <button type="button" onClick={() => handleSubmit()}>
-                                Remove
-                            </button> </div>
-                    </div>
-
-
-                    <div className='tshirt__image'>
-                        {
-                            imageInput.map(
-                                (info, id) => {
-                                    return (
-                                        <tr key={id}>
-                                            {/* <td> {info.fulltext} </td> */}
-                                            <td> {info.image} </td>
-
-
-                                        </tr>
-                                    )
-                                }
-                            )
-                        }
-                    </div>
-                </form>
+                // (e.target.) || setInputData({ ...inputData, name: e.target.[0] })
+              }
+            />
+          </div>
+          <div className="text">
+            <input
+              type="text"
+              placeholder="Add text"
+              name="fulltext"
+              onChange={(e) =>
+                setCustomTshirt({ ...customTshirt, text: e.target.value })
+              }
+            ></input>
+          </div>
+          <div className="submit_btn">
+            <button type="button" onClick={() => handleSubmit()}>
+              Add
+            </button>
+            <div>
+              <button type="button" onClick={() => handleSubmit()}>
+                Remove
+              </button>
             </div>
+          </div>
 
+          <table className="tshirt__image">
+            <tr>
+              <th>Text</th>
+              <th>Image</th>
+            </tr>
 
-            <div className="second__box">
-                <img className="image1" src="https://mms-images-prod.imgix.net/mms/images/catalog/31b9bdaae755e072c7327c8dda1e0930/colors/203900/views/alt/front_medium_extended.png?dpr=1.2&auto=format&nrs=0&w=1000" alt="Image not found" />
+            {tshirts.map((tshirt, id) => {
+              return (
+                <tr key={id}>
+                  <td> {tshirt.text} </td>
+                  <td>
+                    {" "}
+                    <img src={tshirt.image} />{" "}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </form>
+      </div>
 
-                <img className="image2" src="https://mms-images-prod.imgix.net/mms/images/catalog/440393ce75989ee82b2baf426cf57ecb/colors/203901/views/alt/back_medium_extended.png?dpr=1.2&auto=format&nrs=0&w=1000" alt="Image not found" />
-            </div>
-        </div>
-    )
+      <div className="second__box">
+        <img
+          className="image1"
+          src="https://mms-images-prod.imgix.net/mms/images/catalog/31b9bdaae755e072c7327c8dda1e0930/colors/203900/views/alt/front_medium_extended.png?dpr=1.2&auto=format&nrs=0&w=1000"
+          alt="Image not found"
+        />
+
+        <img
+          className="image2"
+          src="https://mms-images-prod.imgix.net/mms/images/catalog/440393ce75989ee82b2baf426cf57ecb/colors/203901/views/alt/back_medium_extended.png?dpr=1.2&auto=format&nrs=0&w=1000"
+          alt="Image not found"
+        />
+      </div>
+    </div>
+  );
 }
-
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 // import StorefrontIcon from '@mui/icons-material/Storefront';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
+import CheckroomIcon from "@mui/icons-material/Checkroom";
 import { Link } from "react-router-dom";
-
-
 
 function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to="/"> <CheckroomIcon /> Fit-Me </Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          {" "}
+          <CheckroomIcon /> Fit-Me{" "}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
         <Nav>
-          <Nav.Link> <Link to="/">  Home</Link> </Nav.Link>
-          <Nav.Link> <Link to="/product2">Products </Link>  </Nav.Link>
-          <Nav.Link ><Link to="/about"> About </Link>   </Nav.Link>
-          <Nav.Link ><Link to="/tshirt"> Custom </Link>   </Nav.Link>
+          <Link to="/"> Home</Link>
+          <Link to="/product2">Products </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/tshirt"> Custom </Link>
 
           {/* <Nav.Link href="#homet"> Home  </Nav.Link>
           <Nav.Link href="#products">Products   </Nav.Link>
